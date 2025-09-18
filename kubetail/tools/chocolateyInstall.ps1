98e54d98d38c9f5270b34aab200d60036e3ab234
Andres Morey <andresmarcel@gmail.com>
2025-09-18 09:48:53 +0300

    release: kubetail-0.7.5
    Signed-off-by: Andres Morey <andresmarcel@gmail.com>

--- kubetail/tools/chocolateyInstall.ps1
+++ kubetail/tools/chocolateyInstall.ps1
@@ -4,12 +4,12 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $osArch = (Get-CimInstance Win32_Processor).Architecture
 
 if ($osArch -eq 12) {
-    $url      = 'https://github.com/kubetail-org/kubetail/releases/download/cli%2Fv0.7.3/kubetail-windows-arm64'
-    $checksum = '59d63dda2d11f5dc55ef120c7dbac7e7e1daa5ffef93f262d7746d624183c564'
+    $url      = 'https://github.com/kubetail-org/kubetail/releases/download/cli%2Fv0.7.5/kubetail-windows-arm64'
+    $checksum = 'a232dee5ecb351c623b6e9bf23b060e71b68739b8b9c00d5915b6dad1994634c'
 }
 else {
-    $url      = 'https://github.com/kubetail-org/kubetail/releases/download/cli%2Fv0.7.3/kubetail-windows-amd64'
-    $checksum = 'e56bfd0418281719e579a83f00ab7d32957ef9739fcc4637de5499f5507a0725'
+    $url      = 'https://github.com/kubetail-org/kubetail/releases/download/cli%2Fv0.7.5/kubetail-windows-amd64'
+    $checksum = '94ea13b25845efb3c7694ea8c87021d0d0014ad683f9aff7eb7e50735125f908'
 }
 
 $packageArgs = @{
